e70a418470fd5538b3f0583354d08ec44daa52db
Roman Voytov <scooterthecat@yandex.ru>
2023-02-16 13:24:44 +0900

    Пункт 5. Добавил аннотации Spring, проверил вроде все корректно работает

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -1,14 +1,21 @@
 package ru.javawebinar.topjava.service;
 
+import org.springframework.stereotype.Service;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 
 import java.util.List;
 
 import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
+
+@Service
 public class MealService {
 
     private MealRepository repository;
+    public MealService(MealRepository repository)
+    {
+        this.repository=repository;
+    }
 
     public Meal create(Meal meal, int userId)
     {
--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -2,6 +2,8 @@ package ru.javawebinar.topjava.web.meal;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.service.MealService;
@@ -16,14 +18,15 @@ import static ru.javawebinar.topjava.web.SecurityUtil.authUserCaloriesPerDay;
 
 import java.util.List;
 
-
+@Controller
 public class MealRestController {
+    @Autowired
     private MealService service;
     private final Logger log = LoggerFactory.getLogger(getClass());
 
     public List<MealTo> getAll()
     {
-        log.info("get all for user{}", authUserId());
+        log.info("get all for user {}", authUserId());
         return MealsUtil.getTos(service.getAll(authUserId()),authUserCaloriesPerDay());
     }
 
@@ -33,13 +36,13 @@ public class MealRestController {
     }
 
     public Meal create(Meal meal) {
-        log.info("create {} for user{}", meal, authUserId());
+        log.info("create {} for user {}", meal, authUserId());
         checkNew(meal);
         return service.create(meal, authUserId());
     }
 
     public void delete(int id) {
-        log.info("delete meal {} for user{}", id, authUserId());
+        log.info("delete meal {} for user {}", id, authUserId());
         service.delete(id,authUserId());
     }
 
