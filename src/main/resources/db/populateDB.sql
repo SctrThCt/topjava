722e8af827b450e99ea9b726d7d0ad8cb4e05f6f
Roman Voytov <scooterthecat@yandex.ru>
2023-02-22 00:33:24 +0900

    Пункт 1. Добавил инициализацию БД для еды. Заполнил ее значениями.

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,3 +1,4 @@
+DELETE FROM user_meal;
 DELETE FROM user_role;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
@@ -10,3 +11,11 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_role (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO user_meal (user_id, date_time, description, calories)
+VALUES (100001,'2022-08-1 14:01:00','Админ ланч', 510),
+       (100001,'2022-08-1 14:02:00','Админ хуянч', 510),
+       (100001,'2022-08-1 14:00:00','Админ ужин', 510),
+       (100000,'2022-08-1 14:02:00','Юзер хуянч', 510),
+       (100000,'2022-08-1 14:01:00','Юзер ланч', 510);
+
