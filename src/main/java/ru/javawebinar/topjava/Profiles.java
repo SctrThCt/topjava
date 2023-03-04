281dc18cd2606acb7c63bb3e7785379cbf96c192
Roman Voytov <scooterthecat@yandex.ru>
2023-03-05 00:18:35 +0900

    Spring data jpa

--- src/main/java/ru/javawebinar/topjava/Profiles.java
+++ src/main/java/ru/javawebinar/topjava/Profiles.java
@@ -5,14 +5,15 @@ import org.springframework.util.ClassUtils;
 public class Profiles {
     public static final String
             JDBC = "jdbc",
-            JPA = "jpa";
+            JPA = "jpa",
+            DATAJPA = "datajpa";
 
-    public static final String REPOSITORY_IMPLEMENTATION = JPA;
+    public static final String REPOSITORY_IMPLEMENTATION = DATAJPA;
 
     public static final String
             POSTGRES_DB = "postgres",
             HSQL_DB = "hsqldb";
-
+    
     //  Get DB profile depending of DB driver in classpath
     public static String getActiveDbProfile() {
         if (ClassUtils.isPresent("org.postgresql.Driver", null)) {
