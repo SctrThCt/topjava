3131285d386bb1c95eeeabcbc02a5ffb3cc4e94e
Roman Voytov <scooterthecat@yandex.ru>
2023-03-07 12:53:57 +0900

    Добаил профили Spring, добавил автоматическое тестирование для всех профилей. Тесты вынесены в базовый класс, вся ининциализация в нем, лог времени тоже. HSQLDB не работает с JBDC, буду чинить

---
+++ src/test/java/ru/javawebinar/topjava/service/DataJpaUserServiceTest.java
@@ -0,0 +1,6 @@
+package ru.javawebinar.topjava.service;
+
+import org.springframework.test.context.ActiveProfiles;
+@ActiveProfiles("datajpa")
+public class DataJpaUserServiceTest extends UserServiceTest {
+}
---
+++ src/test/java/ru/javawebinar/topjava/service/JdbcUserServiceTest.java
@@ -0,0 +1,10 @@
+package ru.javawebinar.topjava.service;
+
+import org.junit.Test;
+import org.springframework.test.context.ActiveProfiles;
+
+
+@ActiveProfiles("jdbc")
+public class JdbcUserServiceTest extends UserServiceTest{
+
+}
